d49d3fe898afbb18646a018b2d12f1acf34b5ac5
Vercel <vercel[bot]@users.noreply.github.com>
2026-03-07 14:39:03 +0000

    Set Up Vercel Speed Insights
    This is an automated pull request generated by Vercel to help install [Speed Insights](https://vercel.com/docs/speed-insights) in your application.
    
    Co-authored-by: Vercel <vercel[bot]@users.noreply.github.com>

--- blog-cientifico/src/main.js
+++ blog-cientifico/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import Particles from "@tsparticles/vue3";
 import { loadSlim } from "@tsparticles/slim";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 import "./style.css";
 
 const app = createApp(App);
@@ -13,3 +14,6 @@ app.use(Particles, {
 });
 
 app.mount("#app");
+
+// Initialize Vercel Speed Insights
+injectSpeedInsights();
